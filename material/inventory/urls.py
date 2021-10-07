fe16c1010f5738aa66e460816c106395e68ea354
Bernat <82199014+bfrangi@users.noreply.github.com>
2021-10-07 12:59:45 +0200

    MAJOR CHANGES
    - Fixed a problem in the "user-items" and "piso-items" views with the orderby and filter functionalities, which redirected back to the home screen instead of simply filtering results.
    - Replaced the "box" field in the "Item" model by a new model called "Box".
    - Created new pages to add/update/remove boxes into the "Box" model.
    - Checked urls of all pages and reorganized them
    - Made it so that there cannot be more than one box with the same combination of number and piso.
    - deleted "box_detail_view.html" (no longer needed; replaced by "box_detail.html").
    - Added link to creating a new box both in the navbar and the side panel
    - Modified some of the views to facilitate the access of attributes
    - updated "box_list.html"

--- material/inventory/urls.py
+++ material/inventory/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path, include
-from .views import ItemListView, UserItemListView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, BoxListView, BoxDetailView, PisoItemListView
+from .views import ItemListView, UserItemListView, ItemDetailView, ItemCreateView, ItemUpdateView, ItemDeleteView, BoxListView, BoxDetailView, PisoItemListView, BoxCreateView, BoxUpdateView, BoxDeleteView
 from . import views
 
 urlpatterns = [
     path('home/', ItemListView.as_view(), name='inventory-home'),
-    path('home/box-list/', BoxListView.as_view(), name='box-list'),
-    path('user/<str:username>/', UserItemListView.as_view(), name='user-items'),
-    path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
-    path('box/<str:piso>/<int:pk>', BoxDetailView.as_view(), name='box-detail'),
     path('item/new/', ItemCreateView.as_view(), name='item-create'),
     path('item/<int:pk>/update/', ItemUpdateView.as_view(), name='item-update'),
     path('item/<int:pk>/delete/', ItemDeleteView.as_view(), name='item-delete'),
+    path('user/<str:username>/', UserItemListView.as_view(), name='user-items'),
+    path('item/<int:pk>/', ItemDetailView.as_view(), name='item-detail'),
     path('info/', views.info, name='inventory-info'),
     path('pisos/', views.pisos, name='pisos-list'),
     path('piso/<str:piso>/', PisoItemListView.as_view(), name='piso-items'),
+
+    path('box/<int:pk>/delete/', BoxDeleteView.as_view(), name='box-delete'),#all these are fine, but they need to recognise duplicate boxes
+    path('box/<int:pk>/update/', BoxUpdateView.as_view(), name='box-update'),
+    path('box/new/', BoxCreateView.as_view(), name='box-create'),
+
+    path('box/<str:piso>/<int:number>/', BoxDetailView.as_view(), name='box-detail'),
+    path('home/box-list/', BoxListView.as_view(), name='box-list'),
 
 ]
--- material/inventory/views.py
+++ material/inventory/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Item, PISOS
+from .models import Item, PISOS, Box
 from django.contrib.auth.models import User
 
 
@@ -10,17 +10,19 @@ from django.contrib.auth.models import User
 #CharField.register_lookup(Lower)
 
 
-def home(request):
-	return render(request, 'inventory/home.html')
-
 def info(request):
 	return render(request, 'inventory/info.html')
+
+def pisos(request):
+    context= {
+    	'pisos_list':PISOS
+    }
+    return render(request, 'inventory/pisos_view.html', context)
 
 class ItemListView(ListView):
 	model = Item
 	template_name = 'inventory/home.html'
 	context_object_name = 'items'
-	#ordering = ['item_name', 'box', '-date_posted']
 	paginate_by = 5
 
 	def get_queryset(self):
@@ -39,6 +41,7 @@ class ItemListView(ListView):
 		context['orderby'] = self.request.GET.get('orderby', 'item_name')
 		return context
 
+
 class PisoItemListView(ListView):
 	model = Item
 	template_name = 'inventory/piso_items.html'
@@ -48,16 +51,15 @@ class PisoItemListView(ListView):
 	def get_queryset(self):
 		this_piso = self.kwargs.get('piso')
 
-		this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == this_piso ]#to get ['Turmalina'] from "TU"
-		this_piso_name = this_piso_name[0]#to get "Turmalina" from ['Turmalina']
+		self.this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == this_piso ]#to get ['Turmalina'] from "TU"
+		self.this_piso_name = self.this_piso_name[0]#to get "Turmalina" from ['Turmalina']
 
-		new_context = Item.objects.filter(piso=this_piso_name)
 		filter_val = self.request.GET.get('filter', '')
 		order = self.request.GET.get('orderby', 'item_name')
 		if filter_val:
-			new_context = new_context.filter(item_name__icontains=filter_val, ).order_by(order)
+			new_context = Item.objects.filter(item_name__icontains=filter_val, ).order_by(order)
 		else:
-			new_context = new_context.order_by(order)
+			new_context = Item.objects.order_by(order)
 		return new_context
 
 	def get_context_data(self, **kwargs):
@@ -93,43 +95,13 @@ class UserItemListView(ListView):
 		context['orderby'] = self.request.GET.get('orderby', 'item_name')
 		return context
 
-class BoxListView(ListView):
-	model = Item
-	template_name = 'inventory/box_view.html'
-	context_object_name = 'items'
-	ordering = ['box']
-	#paginate_by = 5
-
-	def get_queryset(self):
-		this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.request.GET.get('selected_piso', 'TU') ]#to get ['Turmalina'] from "TU"
-		this_piso_name = this_piso_name[0]#to get "Turmalina" from ['Turmalina']
-		new_context = Item.objects.filter(piso=this_piso_name)
-		return new_context
-
-	def get_context_data(self, **kwargs):
-		context = super(BoxListView, self).get_context_data(**kwargs)
-		context['selected_piso'] = self.request.GET.get('selected_piso', 'TU')
-		return context
 
 class ItemDetailView(DetailView):
 	model = Item
 
-class BoxDetailView(ListView):
-	model = Item
-	context_object_name = 'items'
-	ordering = ['item_name']
-	template_name = 'inventory/box_detail_view.html'
-
-	def get_queryset(self):
-		self.this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.kwargs.get('piso') ]#to get ['Turmalina'] from "TU"
-		self.this_piso_name = self.this_piso_name[0]#to get "Turmalina" from ['Turmalina']
-		new_context = Item.objects.filter(piso=self.this_piso_name)
-		return new_context
-
-
 class ItemCreateView(LoginRequiredMixin, CreateView):
 	model = Item
-	fields = ['item_name', 'piso', 'box',]
+	fields = ['item_name', 'box',]
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
@@ -137,7 +109,7 @@ class ItemCreateView(LoginRequiredMixin, CreateView):
 
 class ItemUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Item
-	fields = ['item_name', 'piso', 'box']
+	fields = ['item_name', 'box']
 
 	def form_valid(self, form):
 		form.instance.author = self.request.user
@@ -161,8 +133,141 @@ class ItemDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 		else:
 			return False
 
-def pisos(request):
-    context= {
-    	'pisos_list':PISOS
-    }
-    return render(request, 'inventory/pisos_view.html', context)
+
+class BoxCreateView(LoginRequiredMixin, CreateView):
+	model = Box
+	fields = ['box_number', 'box_name', 'piso',]
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		this_piso_name = [ piso_name[1] for piso_name in PISOS if piso_name[0] == form.instance.piso ]#to get ['TU'] from "Turmalina"
+		this_piso_name = this_piso_name[0]#to get "TU" from ['TU']
+		self.success_url = '/box/' + this_piso_name + '/' + str(form.instance.box_number)
+		return super().form_valid(form)
+
+
+class BoxDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Box
+	success_url = '/home/'
+
+	def test_func(self):
+		box = self.get_object()
+		if self.request.user == box.author:
+			return True
+		else:
+			return False
+
+class BoxUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Box
+	fields = ['box_number', 'box_name', 'piso',]
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		this_piso_name = [ piso_name[1] for piso_name in PISOS if piso_name[0] == form.instance.piso ]#to get ['TU'] from "Turmalina"
+		this_piso_name = this_piso_name[0]#to get "TU" from ['TU']
+		self.success_url = '/box/' + this_piso_name + '/' + str(form.instance.box_number)
+		return super().form_valid(form)
+
+	def test_func(self):
+		box = self.get_object()
+		if self.request.user == box.author:
+			return True
+		else:
+			return False
+
+class BoxDetailView(ListView):
+	model = Item
+	template_name = 'inventory/box_detail.html'
+	context_object_name = 'items'
+	paginate_by = 5
+
+	def get_queryset(self):
+		self.this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.kwargs.get('piso') ]#to get ['Turmalina'] from "TU"
+		self.this_piso_name = self.this_piso_name[0]#to get "Turmalina" from ['Turmalina']
+		self.this_box_number = self.kwargs.get('number')
+		self.this_box = get_object_or_404(Box, box_number=self.this_box_number, piso=self.this_piso_name)
+		new_context = Item.objects.filter(box=self.this_box)
+		return new_context
+
+class BoxListView(ListView):
+	model = Box
+	template_name = 'inventory/box_list.html'
+	context_object_name = 'boxes'
+	ordering = ['box_number']
+	#paginate_by = 5
+
+	def get_queryset(self):
+		self.this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.request.GET.get('selected_piso', 'TU') ]#to get ['Turmalina'] from "TU"
+		self.this_piso_name = self.this_piso_name[0]#to get "Turmalina" from ['Turmalina']
+		new_context = Box.objects.filter(piso=self.this_piso_name)
+		return new_context
+
+	def get_context_data(self, **kwargs):
+		context = super(BoxListView, self).get_context_data(**kwargs)
+		context['selected_piso'] = self.request.GET.get('selected_piso', 'TU')
+		context['items'] = Item.objects.all()
+		return context
+
+##########################################################################
+
+class BoxListView1(ListView):
+	model = Box
+	template_name = 'inventory/box_list.html'
+	context_object_name = 'boxes_and_items'
+	#paginate_by = 5
+
+	def get_context_data(self, **kwargs):
+		context = super(BoxListView1, self).get_context_data(**kwargs)
+		context['selected_piso'] = self.request.GET.get('selected_piso', 'TU')
+		context.update({
+			'boxes': Box.objects.order_by('box_number'),
+			'items': Item.objects.all(),
+		})
+		return context
+
+	def get_queryset(self):
+		self.this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.request.GET.get('selected_piso', 'TU') ]#to get ['Turmalina'] from "TU"
+		self.this_piso_name = self.this_piso_name[0]#to get "Turmalina" from ['Turmalina']
+		new_context = Box.objects.filter(piso=self.this_piso_name).order_by('box_number')
+		return new_context
+
+
+
+
+
+
+
+
+
+class BoxListView2(ListView):
+	model = Item
+	template_name = 'inventory/box_view.html'
+	context_object_name = 'items'
+	ordering = ['box']
+	#paginate_by = 5
+
+	def get_queryset(self):
+		this_piso_name = [ piso_name[0] for piso_name in PISOS if piso_name[1] == self.request.GET.get('selected_piso', 'TU') ]#to get ['Turmalina'] from "TU"
+		this_piso_name = this_piso_name[0]#to get "Turmalina" from ['Turmalina']
+		new_context = Item.objects.filter(piso=this_piso_name)
+		return new_context
+
+	def get_context_data(self, **kwargs):
+		context = super(BoxListView, self).get_context_data(**kwargs)
+		context['selected_piso'] = self.request.GET.get('selected_piso', 'TU')
+		return context
+
+
+
+
+####################  DEPRECATED  #######################
+def home(request):
+	return render(request, 'inventory/home.html')
+
+
+
+
+
+
+
+
