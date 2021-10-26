dd60a76e8e7f8db4d3b27c410405333caff7deb4
Bernat <82199014+bfrangi@users.noreply.github.com>
2021-10-26 08:36:59 +0200

    Fixed urls (they were actually not all fixed before)
    The problem was that the basename hadn't been provided to the rest framework router, so it matched the views based on the queryset, thus messing up the urls for views with the same queryset.

--- material/api/urls.py
+++ material/api/urls.py
@@ -5,8 +5,8 @@ from rest_framework import routers
 from .views import ItemsViewSet, BoxViewSet
 
 router = routers.DefaultRouter()
-router.register(r'items', ItemsViewSet)
-router.register(r'boxes', BoxViewSet)
+router.register(r'items', ItemsViewSet, basename='items')
+router.register(r'boxes', BoxViewSet, basename='boxes')
 
 urlpatterns = [
    path('', include(router.urls)),
