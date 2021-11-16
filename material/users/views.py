9b696ec563c53eff70de3b954ea8e6bc92de7198
materialturmalina <material.turmalina@gmail.com>
2021-11-16 17:12:08 +0100

    correction of typos in success message of profile update

--- material/users/views.py
+++ material/users/views.py
@@ -24,7 +24,7 @@ def profile(request):
 		if u_form.is_valid() and p_form.is_valid():
 			u_form.save()
 			p_form.save()
-			messages.success(request, '¡Tu prefil ha sido actualizado!')
+			messages.success(request, '¡Tu perfil ha sido actualizado!')
 			return redirect('profile')
 	else:
 		u_form = UserUpdateForm(instance=request.user)
